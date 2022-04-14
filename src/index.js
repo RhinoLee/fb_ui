ee557f620a05c15278b7daac9a52f202d2bd92a3
RhinoLee <picklerhino@gmail.com>
2022-04-14 12:51:54 +0800

    大架構 & header complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import "./index.css";
+
+const headerBtns = ["plus", "msg", "notification", "more"]
+
+function closeAllPanels() {
+  const panels = [...document.querySelectorAll(".popover-panel")]
+  panels.forEach(panel => {
+    panel.classList.add("hidden")
+  })
+}
+
+window.addEventListener("click", function () {
+  closeAllPanels()
+})
+
+headerBtns.forEach(idName => {
+  const btn = document.getElementById(`${idName}-btn`);
+  btn.addEventListener("click", function (e) {
+    e.stopPropagation()
+    closeAllPanels()
+    document.getElementById(`${idName}-panel`).classList.remove("hidden")
+  })
+})
